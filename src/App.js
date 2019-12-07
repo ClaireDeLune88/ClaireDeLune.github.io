d906d88d0923458e91d0222dc3da127deb95bb93
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-12-07 13:50:28 +0200

    hatches styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, {useState, useEffect} from "react";
 import { createGlobalStyle } from "styled-components";
+import { StyledApp } from "./AppStyles";
 import { createCalendar } from "./helpers";
 import Hatch from './Hatch';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: center / cover url('./img/calendar_backdrop.jpg');
+    margin: 0;
+  }
+`
 
 function App() {
   const [hatches, setHatches] = useState(createCalendar())
@@ -10,7 +18,10 @@ function App() {
 
   return (
     <>
+      <GlobalStyle />
+      <StyledApp>
       {hatches.map(hatch => <Hatch key={hatch.id} hatchData={hatch} handleClick={handleFlipHatch} />)}
+      </StyledApp>
     </>
   );
 }
--- src/Hatch.js
+++ src/Hatch.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { StyledHatch } from './HatchStyles'
 
-const Hatch = ({hatchData: {id, nr, text, img, open}, handleClick}) => (
-  <>
-    <div className={open ? 'front open' : 'front'}>
+const Hatch = ({ hatchData: { id, nr, text, img, open }, handleClick }) => (
+  <StyledHatch background={img}>
+    <div className={open ? 'front open' : 'front'} onClick={handleClick}>
       <p>{nr}</p>
     </div>
-    <div className={open ? 'back open' : 'back'}>
+    <div className={open ? 'back open' : 'back'} onClick={handleClick}>
       <p>{text}</p>
     </div>
-  </>
+  </StyledHatch>
 )
 
 export default Hatch
