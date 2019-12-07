e4c1c79dfc438d449fb0daad1badd3c6b87bedde
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-12-07 14:45:57 +0200

    persist state in localstorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from "react";
-import { createGlobalStyle } from "styled-components";
-import { StyledApp } from "./AppStyles";
-import { createCalendar } from "./helpers";
-import Hatch from './Hatch';
+import React, { useState, useEffect } from 'react'
+import { createGlobalStyle } from 'styled-components'
+import { StyledApp } from './AppStyles'
+import { createCalendar } from './helpers'
+import Hatch from './Hatch'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -12,18 +12,42 @@ const GlobalStyle = createGlobalStyle`
 `
 
 function App() {
-  const [hatches, setHatches] = useState(createCalendar())
+  const [hatches, setHatches] = useState([])
 
-  const handleFlipHatch = id => console.log(id)
+  useEffect(() => {
+    const calendar = localStorage.calendar
+      ? JSON.parse(localStorage.calendar)
+      : createCalendar()
+
+    setHatches(calendar)
+  }, [])
+
+  // Store calendar in localStorage
+  useEffect(() => {
+    hatches.length && localStorage.setItem('calendar', JSON.stringify(hatches))
+  }, [hatches])
+
+  const handleFlipHatch = id => {
+    const updatedHatches = hatches.map(hatch =>
+      hatch.id === id ? { ...hatch, open: !hatch.open } : hatch
+    )
+    setHatches(updatedHatches)
+  }
 
   return (
     <>
       <GlobalStyle />
       <StyledApp>
-      {hatches.map(hatch => <Hatch key={hatch.id} hatchData={hatch} handleClick={handleFlipHatch} />)}
+        {hatches.map(hatch => (
+          <Hatch
+            key={hatch.id}
+            hatchData={hatch}
+            handleClick={handleFlipHatch}
+          />
+        ))}
       </StyledApp>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Hatch.js
+++ src/Hatch.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { StyledHatch } from './HatchStyles'
 
 const Hatch = ({ hatchData: { id, nr, text, img, open }, handleClick }) => (
-  <StyledHatch background={img}>
-    <div className={open ? 'front open' : 'front'} onClick={handleClick}>
+  <StyledHatch background={img} onClick={() => handleClick(id)}>
+    <div className={open ? 'front open' : 'front'}>
       <p>{nr}</p>
     </div>
-    <div className={open ? 'back open' : 'back'} onClick={handleClick}>
+    <div className={open ? 'back open' : 'back'}>
       <p>{text}</p>
     </div>
   </StyledHatch>
